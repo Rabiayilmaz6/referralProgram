6a74b67ef280f5d518eff3cd476fd8b45181b772
RabiaYilmaz6 <21060999@stu.omu.edu.tr>
2024-12-06 13:41:28 +0300

    Update routes to nest referrals under users and add load_credits action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+    resources :users, only: [:create] do
+        resources :referrals, only: [:index]
+        post 'load_credits', to: 'rewards#load_credits'
+    end
 end
