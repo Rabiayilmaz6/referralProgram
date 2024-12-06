1be40395d7a5a1901ea2c63e429ce38a67892ea3
RabiaYilmaz6 <21060999@stu.omu.edu.tr>
2024-12-06 10:17:14 +0300

    Add reward_type enum and validation for amount in Reward model

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,4 +1,8 @@
 class Reward < ApplicationRecord
   belongs_to :user
   belongs_to :referral
+
+  enum reward_type: { referrer_bonus: 0, referred_bonus: 1 }
+
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
